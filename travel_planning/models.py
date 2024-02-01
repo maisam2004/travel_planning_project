f7228f90dd8ec1e5a0eb4ce0ba978ffd58e86ab3
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-02-01 15:53:15 +0000

    Add user model for handel user login

--- travel_planning/models.py
+++ travel_planning/models.py
@@ -1,6 +1,24 @@
 from travel_planning import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+
+
+
+
 """
 class Category(db.Model):
     #schema for category model
