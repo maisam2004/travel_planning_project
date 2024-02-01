6260345c9667a44a7999a7126a6d9829967b092f
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-02-01 16:21:09 +0000

    Add flask_bcrypt installed files

--- travel_planning/__init__.py
+++ travel_planning/__init__.py
@@ -1,12 +1,12 @@
 import os 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager as login_manager
+from flask_login import LoginManager  
 if os.path.exists("env.py"):
     import env
 
+login_manager = LoginManager()
 app = Flask(__name__)
-
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
--- travel_planning/models.py
+++ travel_planning/models.py
@@ -1,20 +1,19 @@
 from travel_planning import db
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt()  # Using bcrypt for password hashing
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
 
     def set_password(self, password):
-        self.password = generate_password_hash(password)
+        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def check_password(self, password):
-        return check_password_hash(self.password, password)
-
-
+        return bcrypt.check_password_hash(self.password_hash, password)
 
 
 
