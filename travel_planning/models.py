1e9beb122d4a514512a6cf3f5720bc568eb98b6d
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-02-01 15:51:02 +0000

    Update models.py for flask_login for user login

--- travel_planning/models.py
+++ travel_planning/models.py
@@ -1,4 +1,6 @@
 from travel_planning import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 """
 class Category(db.Model):
     #schema for category model
