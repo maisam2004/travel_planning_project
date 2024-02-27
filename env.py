7a554ac54c300c3e2c6f6079d6fef69a1ee1a5d3
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-02-27 23:04:44 +0000

    Setup heroku db add-on for app

--- env.py
+++ env.py
@@ -5,8 +5,10 @@ os.environ.setdefault("PORT","5000")
 os.environ.setdefault("SECRET_KEY","any_secret_key")
 os.environ.setdefault("DEBUG","True")
 os.environ.setdefault("DEVELOPMENT","True")
-os.environ.setdefault("DB_URL","postgresql://postgres:soor1993@localhost/travel_planning")
-os.environ.setdefault("DATABASE_URL","postgresql://postgres:soor1993@localhost/travel_planning")
+#os.environ.setdefault("DB_URL","postgresql://postgres:soor1993@localhost/travel_planning")
+os.environ.setdefault("DATABASE_URL","postgres://jqlmmofnytqpko:1cb0882f86a7ac9d7c9b0750a16d5fb07fe6db9370327b75c0ebb434eda651d7@ec2-52-215-68-14.eu-west-1.compute.amazonaws.com:5432/d6lmh6qmtbmafa")
+#os.environ.setdefault("DATABASE_URL","postgres://$(whoami)")
+
 
 os.environ.setdefault("MAIL_SERVER",'smtp.gmail.com')
 os.environ.setdefault('MAIL_PORT','587')
