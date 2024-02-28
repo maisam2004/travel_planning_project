aa103a3c860ff37cbe5cbb0c5bd6617aa0fea8ff
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-02-28 13:24:34 +0000

    Modify mail app in init file

--- travel_planning/__init__.py
+++ travel_planning/__init__.py
@@ -34,14 +34,14 @@ app.config["SQLALCHEMY_DATABASE_URI"] = uri
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
-mail = Mail()
+
 app.config['MAIL_SERVER'] = os.environ.get("MAIL_SERVER")
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
-
+mail = Mail(app)
 
 
 db = SQLAlchemy(app)
