a2f9b4c32173b2d1cbf607218c2501a56b2a8de4
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-03-08 21:16:27 +0000

    Add style for alert calss danger seperatly

--- env.py
+++ env.py
@@ -10,7 +10,7 @@ import os
 os.environ.setdefault("IP","0.0.0.0")
 os.environ.setdefault("PORT","5000")
 os.environ.setdefault("SECRET_KEY","any_secret_key")
-os.environ.setdefault("DEBUG", "False")
+os.environ.setdefault("DEBUG", "True")
 
 os.environ.setdefault("DEVELOPMENT","True")
 os.environ.setdefault("DATABASE_URL","postgresql://postgres:soor1993@localhost/travel_planning")
